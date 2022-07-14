62e83704634caf5c964c20b6ffeacb55e06df2d7
Joseph Hale <me@jhale.dev>
2022-07-14 02:28:00 +0000

    Implement signature comparisons
    Successfully validates signature equality and compatibility, with the latter
    accounting for subtypes (including Generics!)

--- signatures/__init__.py
+++ signatures/__init__.py
@@ -5,3 +5,88 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 __version__ = "0.1.0"
+
+from typing import *
+import inspect
+
+
+def equal(function: Callable[..., Any], otherFunction: Callable[..., Any]) -> bool:
+    """Determines if two functions have equal signatures.
+
+    Args:
+        function (Callable[..., Any]): The first function to compare
+        otherFunction (Callable[..., Any]): The second function to compare
+
+    Returns:
+        bool: True if both function signatures are equal. False otherwise.
+    """
+    return inspect.signature(function) == inspect.signature(otherFunction)
+
+
+def compatible(function: Callable[..., Any], otherFunction: Callable[..., Any]) -> bool:
+    are_compatible = True
+    signature = inspect.signature(function)
+    otherSignature = inspect.signature(otherFunction)
+
+    for key in signature.parameters.keys():
+        if key in otherSignature.parameters.keys():
+            paramType = signature.parameters[key].annotation
+            otherParamType = otherSignature.parameters[key].annotation
+            equivalent = __are_parameters_equivalent(paramType, otherParamType)
+            are_compatible = are_compatible and equivalent
+        else:
+            are_compatible = False
+    return are_compatible
+
+
+def __are_parameters_equivalent(param1: Type[Any], param2: Type[Any]) -> bool:
+    equal = __are_parameters_equal(param1, param2)
+    subclass = __issubtype(param1, param2)
+    return equal or subclass
+
+
+def __are_parameters_equal(param1: Type[Any], param2: Type[Any]) -> bool:
+    return str(param1) == str(param2)
+
+
+def __issubtype(subtype: Type[Any], supertype: Type[Any]) -> bool:
+    if supertype is None:
+        return subtype is None
+    elif __is_generic(supertype):
+        return (
+            __issubtype(subtype.__bound__, supertype.__bound__)
+            if __is_generic(subtype)
+            else __issubtype(subtype, supertype.__bound__)
+        )
+    else:  # supertype is not directly a Generic
+        if all(__is_parameterized_generic(a) for a in [subtype, supertype]):
+            return __same_base_type(subtype, supertype) and __generic_args_are_subtypes(
+                subtype, supertype
+            )
+        elif all(not __is_parameterized_generic(a) for a in [subtype, supertype]):
+            return issubclass(subtype, supertype)
+        else:  # any(not is_parameterized_generic(a) for a in [subtype, supertype]):
+            return False
+
+
+def __generic_args_are_subtypes(subtype: Type[Any], supertype: Type[Any]) -> bool:
+    return all(
+        __issubtype(key, value)
+        for key, value in dict(zip(subtype.__args__, supertype.__args__)).items()
+    )
+
+
+def __same_base_type(subtype: Type[Any], supertype: Type[Any]) -> bool:
+    return issubclass(subtype.__origin__, supertype.__origin__)
+
+
+def __is_generic(arg: Type[Any]) -> bool:
+    """Returns whether the `arg` is a `Generic` types."""
+    return isinstance(arg, TypeVar)
+
+
+def __is_parameterized_generic(arg: Type[Any]) -> bool:
+    """
+    Returns whether all `args` are concrete types with one or more `Generic` parameters.
+    """
+    return hasattr(arg, "__args__")
--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -3,8 +3,227 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from signatures import __version__
+from typing import *
+
+import signatures
 
 
 def test_version() -> None:
-    assert __version__ == "0.1.0"
+    assert signatures.__version__ == "0.1.0"
+
+
+class Test__FunctionSignatureEquality:
+    class Test__EqualWhen:
+        def test__a_function_is_compared_with_itself(self) -> None:
+            def func(foo: Any) -> None:
+                pass
+
+            assert signatures.equal(func, func)
+
+        def test__two_functions_differ_only_by_name(self) -> None:
+            def foo(thing: Any) -> None:
+                pass
+
+            def bar(thing: Any) -> None:
+                pass
+
+            assert signatures.equal(foo, bar)
+
+        def test__they_use_the_same_generic(self) -> None:
+            T = TypeVar("T")
+
+            def foo(thing: T) -> None:
+                pass
+
+            def bar(thing: T) -> None:
+                pass
+
+            assert signatures.equal(foo, bar)
+
+    class Test__UnequalWhen:
+        def test__they_have_different_return_types(self) -> None:
+            def foo(foo: Any) -> None:
+                pass
+
+            def bar(bar: Any) -> int:
+                pass
+
+            assert not signatures.equal(foo, bar)
+
+        def test__they_have_a_different_parameter_name(self) -> None:
+            def foo(thing1: Any) -> None:
+                pass
+
+            def bar(thing2: Any) -> None:
+                pass
+
+            assert not signatures.equal(foo, bar)
+
+        def test__they_have_a_different_number_of_parameters(self) -> None:
+            def foo(thing1: Any) -> None:
+                pass
+
+            def bar(thing1: Any, thing2: Any) -> None:
+                pass
+
+            assert not signatures.equal(foo, bar)
+
+        def test__they_use_different_generics(self) -> None:
+            T = TypeVar("T")
+            V = TypeVar("V")
+
+            def foo(thing: T) -> None:
+                pass
+
+            def bar(thing: V) -> None:
+                pass
+
+            assert not signatures.equal(foo, bar)
+
+
+class Test__FunctionSignatureCompatibility:
+    class Test__CompatibleGiven:
+        def test__the_same_generic(self) -> None:
+            T = TypeVar("T")
+
+            def foo(thing: T) -> None:
+                pass
+
+            def bar(thing: T) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__a_subclass_of_another_parameters_type(self) -> None:
+            def foo(thing: int) -> None:
+                pass
+
+            def bar(thing: bool) -> None:
+                pass
+
+            assert signatures.compatible(bar, foo)
+
+        def test__an_instance_of_another_parameters_generic(self) -> None:
+            T = TypeVar("T", bound=int)
+
+            def foo(thing: bool) -> None:
+                pass
+
+            def bar(thing: T) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__a_stricter_generic_then_another_parameter(self) -> None:
+            V = TypeVar("V", bound=bool)
+            T = TypeVar("T", bound=int)
+
+            def foo(thing: V) -> None:
+                pass
+
+            def bar(thing: T) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__multiple_identical_parameters(self) -> None:
+            def foo(thing: int, stuff: int) -> None:
+                pass
+
+            def bar(thing: int, stuff: int) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__parameters_using_generic_classes(self) -> None:
+            def foo(thing: List[int]) -> None:
+                pass
+
+            def bar(thing: List[int]) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__parameters_using_generic_classes_with_subtype(self) -> None:
+            def foo(thing: List[bool]) -> None:
+                pass
+
+            def bar(thing: List[int]) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__parameters_using_generic_classes_with_multiple_subtypes(self) -> None:
+            def foo(thing: Tuple[bool, str]) -> None:
+                pass
+
+            def bar(thing: Tuple[int, str]) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+        def test__parameters_using_nested_generic_classes(self) -> None:
+            def foo(thing: List[Tuple[bool, str]]) -> None:
+                pass
+
+            def bar(thing: List[Tuple[int, str]]) -> None:
+                pass
+
+            assert signatures.compatible(foo, bar)
+
+    class Test__IncompatibleGiven:
+        def test__different_parameter_names(self) -> None:
+            def foo(thing1: int) -> None:
+                pass
+
+            def bar(thing2: int) -> None:
+                pass
+
+            assert not signatures.compatible(foo, bar)
+
+        def test__at_least_one_parameter_with_a_different_name(self) -> None:
+            def foo(thing: int, stuff1: int) -> None:
+                pass
+
+            def bar(thing: int, stuff2: int) -> None:
+                pass
+
+            assert not signatures.compatible(foo, bar)
+
+        def test__different_parameter_types(self) -> None:
+            def foo(thing: int) -> None:
+                pass
+
+            def bar(thing: str) -> None:
+                pass
+
+            assert not signatures.compatible(foo, bar)
+
+        def test__an_noninstance_of_a_generic(self) -> None:
+            T = TypeVar("T", bound=int)
+
+            def foo(thing: T) -> None:
+                pass
+
+            def bar(thing: str) -> None:
+                pass
+
+            assert not signatures.compatible(bar, foo)
+
+        def test__parameters_using_different_generic_subtypes(self) -> None:
+            def foo(thing: List[int]) -> None:
+                pass
+
+            def bar(thing: List[Tuple[int, str]]) -> None:
+                pass
+
+            assert not signatures.compatible(foo, bar)
+
+        def test__parameters_using_different_nested_generic_classes(self) -> None:
+            def foo(thing: List[Tuple[int, bool]]) -> None:
+                pass
+
+            def bar(thing: List[Tuple[int, str]]) -> None:
+                pass
+
+            assert not signatures.compatible(foo, bar)
